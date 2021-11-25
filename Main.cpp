9063b08907fac982e6b3ac8206bb734fa930db99
Ivandows <tal200@lenta.ru>
2021-11-25 11:44:14 +0700

    main

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,22 @@
 //
 
 #include <iostream>
+#include "HashTree.h"
+HashTree h(5);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    h.Add(5);
+    h.Add(2);
+    h.Add(3);
+    h.Add(7);
+    h.Add(4);    
+    h.Add(8);
+    h.Add(15);
+    h.Add(10);
+
+
+    h.Vivod();
+    //std::cout << h.Find(10)->data;    
 }
